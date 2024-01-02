ae1f09aaabdbb25f24539d6f0346ddfc7a0758dd
Emauri-Neto <emaurigomesneto@gmail.com>
2024-01-02 16:22:47 -0300

    feat: adding foodharbor path

---
+++ authuser/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .models import User
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['name'] = user.name
+        token['email'] = user.email
+
+        return token
--- authuser/views.py
+++ authuser/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework_simplejwt.views import TokenObtainPairView
+from authuser.serializers import MyTokenObtainPairSerializer
 
-# Create your views here.
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
